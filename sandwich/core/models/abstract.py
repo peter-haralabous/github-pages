b00416b4d60a4c660f93353181daf1ace4936e4d
Raoul George <raoulgeorge@gmail.com>
2025-09-23 09:57:37 -0700

    feat: add base model
    for now it's only Timestamped + UUID
    
    https://new-hippo.atlassian.net/browse/HC-40

--- sandwich/core/models/abstract.py
+++ sandwich/core/models/abstract.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.db import models
 
 
@@ -5,6 +7,18 @@ class TimestampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
+    class Meta:
+        abstract = True
+
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class BaseModel(TimestampedModel, UUIDModel):
     class Meta:
         abstract = True
 
