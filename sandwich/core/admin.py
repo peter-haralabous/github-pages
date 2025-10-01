be4995bdbfece8208020e45465a202cf3e0685ae
Paul Shannon <paul.shannon@thrive.health>
2025-10-01 10:51:41 -0400

    feat: register core models in admin
    Refs: https://new-hippo.atlassian.net/browse/HC-35

--- sandwich/core/admin.py
+++ sandwich/core/admin.py
@@ -1,1 +1,12 @@
-# Register your models here.
+from django.contrib import admin
+
+from sandwich.core.models import Encounter
+from sandwich.core.models import Invitation
+from sandwich.core.models import Organization
+from sandwich.core.models import Patient
+from sandwich.core.models import Task
+from sandwich.core.models import Template
+
+admin.site.register(
+    [Encounter, Invitation, Organization, Patient, Task, Template],
+)
