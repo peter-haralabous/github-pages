1549e0013aca77da947d6e5bea0db028be9052a0
Paul Shannon <paul.shannon@thrive.health>
2025-10-14 11:09:58 -0400

    docs: add namespace notes to urls
    Refs: https://new-hippo.atlassian.net/browse/HC-52

--- config/urls.py
+++ config/urls.py
@@ -5,14 +5,15 @@ from django.urls import URLResolver
 from django.urls import include
 from django.urls import path
 
+# URLs should be added to a namespace if at all possible to allow consistent targeting by middleware
+# See: ConsentMiddleware, PatientAccessMiddleware
 urlpatterns: list[URLResolver | URLPattern] = [
-    # Django Admin, use {% url 'admin:index' %}
+    # namespace=admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
-    # User management
     path("users/", include("sandwich.users.urls", namespace="users")),
+    # NO NAMESPACE; allauth doesn't support; bad allauth
     path("accounts/", include("allauth.urls")),
     path("accounts/", include("allauth.socialaccount.urls")),
-    # Anymail webhooks
     path("anymail/", include("anymail.urls", namespace="anymail")),
     path("patients/", include("sandwich.patients.urls", namespace="patients")),
     path("providers/", include("sandwich.providers.urls", namespace="providers")),
@@ -23,6 +24,7 @@ urlpatterns: list[URLResolver | URLPattern] = [
 if settings.SERVE_DJDT:
     import debug_toolbar
 
+    # namespace=djdt
     urlpatterns = [
         path("__debug__/", include(debug_toolbar.urls)),
         *urlpatterns,
