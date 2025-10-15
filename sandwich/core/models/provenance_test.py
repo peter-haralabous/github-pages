a290a0ea7a6a0b71cf57cc892822030eb108cf4c
Raoul George <raoulgeorge@gmail.com>
2025-10-15 11:58:13 -0700

    test: add coverage for user-provenance linking
    https://new-hippo.atlassian.net/browse/HC-88

--- sandwich/core/models/provenance_test.py
+++ sandwich/core/models/provenance_test.py
@@ -1,14 +1,17 @@
 import datetime
 
+import django.db
 import pytest
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from sandwich.core.models import Document
 from sandwich.core.models import Patient
 from sandwich.core.models import Provenance
+from sandwich.core.models.provenance import SourceTypes
+from sandwich.users.models import User
 
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_provenance_url_property():
     patient = Patient.objects.create(date_of_birth=datetime.date(2000, 1, 1))
     test_file = SimpleUploadedFile("doc.pdf", b"dummy content")
@@ -28,3 +31,27 @@ def test_provenance_url_property():
     # Provenance with no document
     prov3 = Provenance(document=None)
     assert prov3.url is None
+
+
+@pytest.mark.django_db(transaction=True)
+def test_provenance_user_constraint_text():
+    patient = Patient.objects.create(date_of_birth=datetime.date(2000, 1, 1))
+    test_file = SimpleUploadedFile("doc.pdf", b"dummy content")
+    doc = Document.objects.create(patient=patient, file=test_file)
+    # User not provided
+    with pytest.raises(django.db.utils.IntegrityError):
+        Provenance.objects.create(document=doc, source_type=SourceTypes.TEXT, user=None)
+
+    # User is provided
+    user = User.objects.create()
+    prov = Provenance.objects.create(document=doc, source_type=SourceTypes.TEXT, user=user)
+    assert prov.user == user
+
+
+@pytest.mark.django_db
+def test_provenance_user_constraint_not_text():
+    patient = Patient.objects.create(date_of_birth=datetime.date(2000, 1, 1))
+    test_file = SimpleUploadedFile("doc.pdf", b"dummy content")
+    doc = Document.objects.create(patient=patient, file=test_file)
+    prov = Provenance.objects.create(document=doc, source_type=SourceTypes.DOCUMENT, user=None)
+    assert prov.user is None
