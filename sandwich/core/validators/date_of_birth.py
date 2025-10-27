ba68568928543caa44c17a7846ed075f6eebed56
Paul Shannon <paul.shannon@thrive.health>
2025-10-27 10:46:37 -0400

    feat: validators
    Refs: https://new-hippo.atlassian.net/browse/HC-117

--- sandwich/core/validators/date_of_birth.py
+++ sandwich/core/validators/date_of_birth.py
@@ -2,17 +2,13 @@ from datetime import date
 from typing import Any
 
 from django.forms import ValidationError
-from django.utils import timezone
+
+from sandwich.core.validators.date_time import not_in_future
 
 
 def valid_date_of_birth(value: Any) -> date:
     if not isinstance(value, date):
         msg = "Must be a valid date."
         raise ValidationError(msg)
-
-    # Using django timezone means we're comparing against the user's timezone
-    if value > timezone.now().date():
-        msg = "Date cannot be in the future."
-        raise ValidationError(msg)
-
+    not_in_future(value)
     return value
