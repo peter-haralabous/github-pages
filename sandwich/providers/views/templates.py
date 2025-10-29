273dc7f474bc46b960dfd31d8756798247e31608
Robin Jastrzebski <robin.jastrzebski@thrive.health>
2025-10-29 15:42:42 -0700

    refactor: form_list view to use new authorize_objects decorator
    now with tests
    
    https://new-hippo.atlassian.net/browse/HC-75

--- sandwich/providers/views/templates.py
+++ sandwich/providers/views/templates.py
@@ -2,12 +2,10 @@ import logging
 
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
 from sandwich.core.models import Form
 from sandwich.core.models import Organization
-from sandwich.core.service.organization_service import get_provider_organizations
 from sandwich.core.service.permissions_service import ObjPerm
 from sandwich.core.service.permissions_service import authorize_objects
 from sandwich.core.util.http import AuthenticatedHttpRequest
@@ -20,23 +18,22 @@ logger = logging.getLogger(__name__)
 def templates_home(request: AuthenticatedHttpRequest, organization: Organization):
     """Provider template landing page to choose to manage form or summary templates."""
     logger.info(
-        "Accessing organization templates", extra={"user_id": request.user.id, "organization_id": organization.id}
+        "Accessing organization templates home", extra={"user_id": request.user.id, "organization_id": organization.id}
     )
     return render(request, "provider/templates.html", {"organization": organization})
 
 
-# TODO: Permissions
 @login_required
-def form_list(request: AuthenticatedHttpRequest, organization_id: int):
+@authorize_objects([ObjPerm(Organization, "organization_id", ["view_organization"])])
+def form_list(request: AuthenticatedHttpRequest, organization: Organization):
     """Provider view of form templates for an organization.
 
     Displays the list of available forms for the organization.
     """
     logger.info(
         "Accessing organization form list",
-        extra={"user_id": request.user.id, "organization_id": organization_id},
+        extra={"user_id": request.user.id, "organization_id": organization.id},
     )
-    organization = get_object_or_404(get_provider_organizations(request.user), id=organization_id)
     forms = Form.objects.filter(organization=organization)
 
     page = request.GET.get("page", 1)
--- sandwich/providers/views/templates_test.py
+++ sandwich/providers/views/templates_test.py
@@ -14,3 +14,18 @@ def test_templates_home_user_not_in_organization_deny_access(client, user, organ
     url = reverse("providers:templates_home", kwargs={"organization_id": organization.id})
     response = client.get(url)
     assert response.status_code == 404
+
+
+def test_form_list(client, provider, organization):
+    client.force_login(provider)
+    url = reverse("providers:form_templates_list", kwargs={"organization_id": organization.id})
+    response = client.get(url)
+    assert response.status_code == 200
+    assert "provider/form_list.html" in [t.name for t in response.templates]
+
+
+def test_form_list_user_not_in_organization_deny_access(client, user, organization):
+    client.force_login(user)
+    url = reverse("providers:form_templates_list", kwargs={"organization_id": organization.id})
+    response = client.get(url)
+    assert response.status_code == 404
