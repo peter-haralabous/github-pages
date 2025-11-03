f438deaa377271615202183b1c237beb1bbbe949
Robin Jastrzebski <robin.jastrzebski@thrive.health>
2025-11-03 15:34:12 -0800

    refactor: reduce nesting
    form.organization is not optional, no need for if check
    
    https://new-hippo.atlassian.net/browse/HC-147

--- sandwich/core/service/form_service.py
+++ sandwich/core/service/form_service.py
@@ -10,20 +10,19 @@ def assign_default_form_permissions(form: Form) -> None:
     Since STAFF can't create forms, they only get view_form permissions.
     """
     # apply org-wide role perms
-    if form.organization:
-        org = form.organization
-        owner_role = org.get_role(RoleName.OWNER)
-        admin_role = org.get_role(RoleName.ADMIN)
-        staff_role = org.get_role(RoleName.STAFF)
+    org = form.organization
+    owner_role = org.get_role(RoleName.OWNER)
+    admin_role = org.get_role(RoleName.ADMIN)
+    staff_role = org.get_role(RoleName.STAFF)
 
-        # All org staff can view forms in their org.
-        assign_perm("view_form", owner_role.group, form)
-        assign_perm("view_form", admin_role.group, form)
-        assign_perm("view_form", staff_role.group, form)
+    # All org employee roles can view forms in their org.
+    assign_perm("view_form", owner_role.group, form)
+    assign_perm("view_form", admin_role.group, form)
+    assign_perm("view_form", staff_role.group, form)
 
-        # Only OWNER and ADMIN can change/delete forms.
-        assign_perm("change_form", owner_role.group, form)
-        assign_perm("change_form", admin_role.group, form)
+    # Only OWNER and ADMIN can change/delete forms.
+    assign_perm("change_form", owner_role.group, form)
+    assign_perm("change_form", admin_role.group, form)
 
-        assign_perm("delete_form", owner_role.group, form)
-        assign_perm("delete_form", admin_role.group, form)
+    assign_perm("delete_form", owner_role.group, form)
+    assign_perm("delete_form", admin_role.group, form)
