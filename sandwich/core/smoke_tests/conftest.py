0067e8a5d24baaede707f247aada84999c90c448
Neil Gentleman <ngentleman@gmail.com>
2025-11-06 08:31:27 -0800

    chore: spelling

--- sandwich/core/smoke_tests/conftest.py
+++ sandwich/core/smoke_tests/conftest.py
@@ -3,5 +3,5 @@ import pytest
 
 @pytest.fixture(scope="session")
 def base_url():
-    """Hard coded to the integration env for now, but could potentially be paramaterized in the future"""
+    """Hard coded to the integration env for now, but could potentially be parameterized in the future"""
     return "https://hc.wethrive.ninja"
