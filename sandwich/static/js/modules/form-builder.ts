3c08a0e5430d0162f56491f173227381a0f8f1d3
Jeffrey Larson <jeffrey.larson@thrive.health>
2025-11-07 13:33:59 -0800

    feat: load license for survey-creator to hide its banner
    Ref: https://new-hippo.atlassian.net/browse/HC-74

--- sandwich/static/js/modules/form-builder.ts
+++ sandwich/static/js/modules/form-builder.ts
@@ -1,10 +1,19 @@
 import { SurveyCreator } from 'survey-creator-js';
+import { getSurveyLicenseKey } from '../lib/survey-license-keys';
+import { slk } from 'survey-core';
+
+const ENVIRONMENT = JSON.parse(
+  document.getElementById('environment')?.textContent || '',
+);
 
 document.addEventListener('DOMContentLoaded', () => {
   const creatorOptions = {
     autoSaveEnabled: true,
     collapseOnDrag: true,
   };
+
+  const licenseKey = getSurveyLicenseKey(ENVIRONMENT);
+  if (licenseKey) slk(licenseKey);
 
   const creator = new SurveyCreator(creatorOptions);
   const creatorContainer = document.getElementById('form-builder-container');
