b3a4b9479d4e0163d0e3d9d9b04fe585f04a2191
Jimmy Fung <jimmy.fung@thrive.health>
2025-11-25 18:26:45 -0800

    feat: attachment api

---
+++ sandwich/core/views/attachment.py
@@ -0,0 +1,61 @@
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+from django.http import HttpResponseForbidden
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_POST
+from django_jsonform.views import login_required
+
+from sandwich.core.models.attachment import Attachment
+from sandwich.core.service.permissions_service import ObjPerm
+from sandwich.core.service.permissions_service import authorize_objects
+from sandwich.core.util.http import AuthenticatedHttpRequest
+
+
+@require_POST
+@login_required
+def attachment_upload(request: AuthenticatedHttpRequest) -> HttpResponse:
+    uploaded = request.FILES.get("file-upload")
+    if not uploaded:
+        return HttpResponseBadRequest("No file was uploaded.")
+
+    # TODO: do we have a file size limit imposed on the infra side?
+    if uploaded.size and uploaded.size > 10 * 1024 * 1024:  # 10MB
+        return HttpResponseBadRequest("File is too large.")
+
+    attachment = Attachment.objects.create(
+        file=uploaded,
+        uploaded_by=request.user,
+        original_filename=uploaded.name or "untitled",
+        content_type=uploaded.content_type or "application/octet-stream",
+    )
+
+    return JsonResponse(
+        {
+            "id": attachment.pk,
+            "url": attachment.file.url,
+            "original_filename": attachment.original_filename,
+            "content_type": attachment.content_type,
+        }
+    )
+
+
+@require_http_methods(["DELETE"])
+@login_required
+def attachment_delete(request: AuthenticatedHttpRequest, attachment_id: str) -> HttpResponse:
+    attachment = Attachment.objects.get(id=attachment_id)
+    if request.user.id == attachment.uploaded_by.id and attachment.uploaded_by.has_perm(
+        "delete_attachment", attachment
+    ):
+        attachment.delete()
+        return HttpResponse(status=204)
+
+    return HttpResponseForbidden()
+
+
+@require_GET
+@login_required
+@authorize_objects([ObjPerm(Attachment, "attachment_id", ["view_attachment"])])
+def attachment_by_id(request: AuthenticatedHttpRequest, attachment: Attachment) -> HttpResponse:
+    return JsonResponse({"url": attachment.file.url})
