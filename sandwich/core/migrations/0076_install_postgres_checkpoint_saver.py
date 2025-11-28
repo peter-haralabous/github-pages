86e6d35b12619ade8fd860a4b719cbf51c38cfec
Paul Shannon <paul.shannon@thrive.health>
2025-11-28 12:48:48 -0500

    fix: revert "fix" from will
    Refs: https://new-hippo.atlassian.net/browse/HC-269

--- sandwich/core/migrations/0076_install_postgres_checkpoint_saver.py
+++ sandwich/core/migrations/0076_install_postgres_checkpoint_saver.py
@@ -1,40 +1,19 @@
-from contextlib import contextmanager
-
+from django.conf import settings
 from django.db import migrations
 from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.store.postgres import PostgresStore
-from psycopg.rows import dict_row
-
-# NOTE-NG: we override the default langgraph postgres backends to avoid `binary=True` on the cursors that it creates.
-# This avoids the following error:
-# > psycopg.NotSupportedError: client-side cursors don't support binary results
-
-
-class CustomPostgresSaver(PostgresSaver):
-    @contextmanager
-    def _cursor(self, *, pipeline: bool = False):
-        assert not pipeline, "Pipeline mode is not supported."
-        with self.lock, self.conn as conn, conn.cursor(row_factory=dict_row) as cursor:
-            yield cursor
-
-
-class CustomPostgresStore(PostgresStore):
-    @contextmanager
-    def _cursor(self, *, pipeline: bool = False):
-        assert not pipeline, "Pipeline mode is not supported."
-        with self.conn as conn, conn.cursor(row_factory=dict_row) as cursor:
-            yield cursor
 
 
 def install_postgres_checkpoint_saver(apps, schema_editor):
-    connection = schema_editor.connection.connection
-    CustomPostgresSaver(connection).setup()
-    CustomPostgresStore(connection).setup()
+    with (
+        PostgresSaver.from_conn_string(settings.DATABASE_URL) as checkpointer,
+        PostgresStore.from_conn_string(settings.DATABASE_URL) as store,
+    ):
+        checkpointer.setup()
+        store.setup()
 
 
 class Migration(migrations.Migration):
-    atomic = False
-
     dependencies = [
         ("core", "0075_backfill_completed_form_submissions"),
     ]
